7c9ef086bb21efd5705ce303de8ccbb4a89fadca
Nassif Nassif <nassif.nassif@vzbv.de>
2025-01-20 16:03:11 +0100

    reset projecgt

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
-import gluestackPlugin from '@gluestack-ui/nativewind-utils/tailwind-plugin';
+const gluestackPlugin = require('@gluestack-ui/nativewind-utils/tailwind-plugin')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "media",
-  content: ["app/**/*.{tsx,jsx,ts,js}", "components/**/*.{tsx,jsx,ts,js}"],
+  darkMode: 'media',
+  content: ['./app/**/*.{tsx,jsx,ts,js}', './components/**/*.{tsx,jsx,ts,js}'],
   presets: [require('nativewind/preset')],
   safelist: [
     {
       pattern:
-        /(bg|border|text|stroke|fill)-(primary|secondary|tertiary|error|success|warning|info|typography|outline|background|indicator)-(0|50|100|200|300|400|500|600|700|800|900|950|white|gray|black|error|warning|muted|success|info|light|dark|primary)/,
+        /(bg|border|text|stroke|fill)-(primary|secondary|tertiary|error|success|warning|info|typography|outline|background)-(0|50|100|200|300|400|500|600|700|800|900|950|white|gray|black|error|warning|muted|success|info|light|dark)/,
     },
   ],
   theme: {
@@ -195,4 +195,4 @@ module.exports = {
     },
   },
   plugins: [gluestackPlugin],
-};
+}
