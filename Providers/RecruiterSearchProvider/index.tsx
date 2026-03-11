f170fc24c1e179d2301b8c30552afa13f77828b4
Nassif <nassif.najdat@gmail.com>
2026-03-11 12:27:23 +0100

    Add search provider that will have the search details and crew list

---
+++ Providers/RecruiterSearchProvider/index.tsx
@@ -0,0 +1,104 @@
+import { createContext, useContext } from 'react'
+import { useLocalSearchParams } from 'expo-router'
+import { useQueries } from '@tanstack/react-query'
+import { TRecruiterSearch, TCrewSimple } from '@/api/types'
+import { getRecruiterSearchById, getCrewList } from '@/api'
+import { useTranslation } from 'react-i18next'
+import { useUser, ActiveProfile } from '@/Providers/UserProvider'
+
+type TSearchQueryState<T> = {
+  data?: T
+  isLoading: boolean
+  isSuccess: boolean
+  isError: boolean
+  isRefetching: boolean
+  error: Error | null
+  refetch: () => void
+}
+
+type TRecruiterSearchContext = {
+  search: TSearchQueryState<TRecruiterSearch>
+  crewList: TSearchQueryState<TCrewSimple[]>
+  refetchAll: () => void
+}
+
+// const SearchContext = createContext<TRecruiterSearchContext>({
+//   isLoading: false,
+//   isLoadingSearch: false,
+//   isLoadingCrewList: false,
+//   refetchCrewList: () => {},
+// })
+
+const defaultQueryState = <T,>(): TSearchQueryState<T> => ({
+  data: undefined,
+  isLoading: false,
+  isSuccess: false,
+  isError: false,
+  isRefetching: false,
+  error: null,
+  refetch: () => {},
+})
+
+const SearchContext = createContext<TRecruiterSearchContext>({
+  search: defaultQueryState<TRecruiterSearch>(),
+  crewList: defaultQueryState<TCrewSimple[]>(),
+  refetchAll: () => {},
+})
+
+export const useRecruiterSearch = () => useContext(SearchContext)
+
+const RecruiterSearchProvider = ({ children }: React.PropsWithChildren) => {
+  const {
+    i18n: { language },
+  } = useTranslation()
+
+  const { searchId } = useLocalSearchParams<{ searchId: string }>()
+  const { activeProfile } = useUser()
+  const { token } = activeProfile as ActiveProfile
+
+  const [searchQuery, crewListQuery] = useQueries({
+    queries: [
+      {
+        queryKey: ['recruiter-search-by-id', searchId, language],
+        queryFn: () => getRecruiterSearchById(searchId, token, language),
+      },
+      {
+        queryKey: ['recruiter-crew-list', searchId, language],
+        queryFn: () => getCrewList(searchId as string, token, language),
+      },
+    ],
+  })
+
+  return (
+    <SearchContext.Provider
+      value={{
+        search: {
+          data: searchQuery.data?.[0],
+          isLoading: searchQuery.isLoading,
+          isSuccess: searchQuery.isSuccess,
+          isError: searchQuery.isError,
+          isRefetching: searchQuery.isRefetching,
+          error: searchQuery.error,
+          refetch: searchQuery.refetch,
+        },
+        crewList: {
+          data: crewListQuery.data,
+          isLoading: crewListQuery.isLoading,
+          isSuccess: crewListQuery.isSuccess,
+          isError: crewListQuery.isError,
+          isRefetching: crewListQuery.isRefetching,
+          error: crewListQuery.error,
+          refetch: crewListQuery.refetch,
+        },
+        refetchAll: () => {
+          searchQuery.refetch()
+          crewListQuery.refetch()
+        },
+      }}
+    >
+      {children}
+    </SearchContext.Provider>
+  )
+}
+
+export default RecruiterSearchProvider
--- app/(tabs)/recruiter/search/[searchId]/_layout.tsx
+++ app/(tabs)/recruiter/search/[searchId]/_layout.tsx
@@ -3,42 +3,45 @@ import { NavBar } from '@/components/appUI'
 import { useTranslation } from 'react-i18next'
 import ContactSupport from '@/components/common/ContactSupport'
 import { supportTeam } from '@/api'
+import SearchProvider from '@/Providers/RecruiterSearchProvider'
 
 export default function OfferDetailLayout() {
   const { t } = useTranslation(['screens-labels'])
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        header: (props) => (
-          <NavBar
-            {...props}
-            rightAction={<ContactSupport title={t('contact-support', { ns: 'common' })} supportTeam={supportTeam} />}
-          />
-        ),
-        contentStyle: { backgroundColor: 'white' },
-      }}
-    >
-      {/* Offer details */}
-      <Stack.Screen
-        name="index"
-        options={({ route }) => {
-          const { params } = route
-          const { searchId } = params as { searchId: string }
-          return {
-            headerShown: true,
-            title: t('search'),
-          }
-        }}
-      />
-
-      {/* Crew section */}
-      <Stack.Screen
-        name="crew"
-        options={{
+    <SearchProvider>
+      <Stack
+        screenOptions={{
           headerShown: false,
+          header: (props) => (
+            <NavBar
+              {...props}
+              rightAction={<ContactSupport title={t('contact-support', { ns: 'common' })} supportTeam={supportTeam} />}
+            />
+          ),
+          contentStyle: { backgroundColor: 'white' },
         }}
-      />
-    </Stack>
+      >
+        {/* Offer details */}
+        <Stack.Screen
+          name="index"
+          options={({ route }) => {
+            const { params } = route
+            const { searchId } = params as { searchId: string }
+            return {
+              headerShown: true,
+              title: t('search'),
+            }
+          }}
+        />
+
+        {/* Crew section */}
+        <Stack.Screen
+          name="crew"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </SearchProvider>
   )
 }
