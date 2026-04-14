6f43f8da0efec4433ef85af64f7edc6e728b432e
Nassif <n_nassif@icloud.com>
2026-04-14 10:35:58 +0200

    Open register links in in-app browser using expo-web-browser

--- components/appUI/LoginFormLinks/index.tsx
+++ components/appUI/LoginFormLinks/index.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react'
+import * as WebBrowser from 'expo-web-browser'
 import { Link, LinkText, VStack } from '@/components/ui'
 import { useTranslation } from 'react-i18next'
 
@@ -10,15 +11,18 @@ interface ILoginFormLinksProps {
 const LoginFormLinks: FC<ILoginFormLinksProps> = ({ isCrew, isRecruiter }) => {
   const { t } = useTranslation('login-screen')
 
+  const handleRegisterAsRecruiter = () => WebBrowser.openBrowserAsync('https://www.marineria.it/En/Rec/Reg.aspx')
+  const handleRegisterAsCrew = () => WebBrowser.openBrowserAsync('https://www.marineria.it/En/Pro/Reg.aspx')
+
   return (
     <VStack className="flex items-start rounded-md bg-background-100 justify-items-start">
       {!isRecruiter && (
-        <Link href="https://www.marineria.it/En/Rec/Reg.aspx" className="mb-1">
+        <Link onPress={handleRegisterAsRecruiter} className="mb-1">
           <LinkText>{t('register-as-recruiter')}</LinkText>
         </Link>
       )}
       {!isCrew && (
-        <Link href="https://www.marineria.it/En/Pro/Reg.aspx">
+        <Link onPress={handleRegisterAsCrew}>
           <LinkText>{t('register-as-crew')}</LinkText>
         </Link>
       )}
